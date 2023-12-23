20f208d60591e84c8f6be9076252700278fc80d7
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-12-23 09:32:38 +0200

    started

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="bg-[#4221B0] h-screen px-[68px]">index</div>
+  )
+}
+
+export default Home
---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/home"
+
+
+function Index() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+}
+
+export default Index;
