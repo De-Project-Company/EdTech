f64dc4bce778361ab3e721c3502395445ad4c368
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-20 10:29:58 +0100

    Update ongoing.tsx

--- components/teacher/assessments/ongoing.tsx
+++ components/teacher/assessments/ongoing.tsx
@@ -11,7 +11,7 @@ import {
 
 function page() {
   return (
-    <div className="bg-[#4221B0] h-[515px] overflow-y-auto py-[24px] px-[18px] rounded-[12px] mt-[24px] w-[940px] space-y-5">
+    <div className="bg-[#4221B0] h-[515px] overflow-y-auto overflow-x-hidden py-[24px] px-[18px] rounded-[12px] mt-[24px] w-[940px] space-y-5">
       <Card {...card1} />
       <Card {...card2} />
       <Card {...card3} />
