a523efd2e26d67fa6abc337fc763013772bbf2bf
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-02-20 00:11:24 +0000

    nw

--- app/(Teachers)/teachers-student-fees/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Admins = () => {
-  return <div>Admins</div>;
-};
-
-export default Admins;
--- utils/twcx.ts
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx';
-import { ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export default function cn(...classes: ClassValue[]) {
-  return twMerge(clsx(...classes));
-}
